7aeef08b733fcf08a2a5e45f6e71bf6d3578fd88
silkstring <why612@naver.com>
2023-02-17 14:36:16 +0900

    테이블 수정

--- sql_script/20230216_member_tbl_system_1234.sql
+++ sql_script/20230216_member_tbl_system_1234.sql
@@ -1,14 +1,41 @@
-drop table member_tbl_02;
-create table member_tbl_02(
-    custno number(6) not null primary key,
-    custname varchar2(20),
-    phone varchar2(13),
-    address varchar2(60),
-    joindate date,
-    grade char(1),
-    city char(2)
+DROP TABLE "MEMBER_TBL_02";
+
+CREATE TABLE "MEMBER_TBL_02" (
+	"CUSTNO"	NUMBER(6)		NOT NULL,
+	"CUSTNAME"	VARCHAR2(20),
+	"PHONE"	    VARCHAR2(13),
+	"ADDRESS"	VARCHAR2(60),
+	"JOINDATE"	DATE,
+	"GRADE"	    CHAR(1),
+	"CITY"  	CHAR(2)
 );
-SELECT * FROM member_tbl_02;
+
+DROP TABLE "MONEY_TBL_02";
+
+CREATE TABLE "MONEY_TBL_02" (
+	"SALENO"	NUMBER(8)		NOT NULL,
+	"CUSTNO"	NUMBER(6)		NOT NULL,
+	"PCOST"	    NUMBER(8),
+	"AMOUNT"	NUMBER(4),
+	"PRICE"	    NUMBER(8),
+	"PCODE" 	VARCHAR2(4),
+	"DATE"	    DATE
+);
+
+ALTER TABLE "MEMBER_TBL_02" ADD CONSTRAINT "PK_MEMBER_TBL_02" PRIMARY KEY (
+	"CUSTNO"
+);
+
+ALTER TABLE "MONEY_TBL_02" ADD CONSTRAINT "PK_MONEY_TBL_02" PRIMARY KEY (
+	"SALENO"
+);
+
+ALTER TABLE "MONEY_TBL_02" ADD CONSTRAINT "FK_MEMBER_TBL_02_TO_MONEY_TBL_02_1" FOREIGN KEY (
+	"CUSTNO"
+)
+REFERENCES "MEMBER_TBL_02" (
+	"CUSTNO"
+);
 
 insert all
     into member_tbl_02 values(100001, '김행복', '010-1111-2222', '서울 동대문구 휘경1동', '2015-12-02', 'A', '01')
@@ -19,30 +46,19 @@ insert all
     into member_tbl_02 values(100006, '차공단', '010-1111-7777', '제주도 제주시 감나무골', '2015-12-11', 'C', '60')
 select * from dual;
 commit;
-    
-
-drop table money_tbl_02;
-create table money_tbl_02(
-    custno number(6) not null,
-    saleno number(8) not null primary key,
-    pcost number(8),
-    amount number(4),
-    price number(8),
-    pcode varchar2(4),
-    sdate date,
-    constraint fk_custno foreign key (custno) references member_tbl_02 (custno)
-);
-SELECT * FROM money_tbl_02;
+SELECT * FROM MEMBER_TBL_02;
 
 insert all
-    into money_tbl_02 values(100001, 20160001, 500, 5, 2500, 'A001', '2016-01-01')
-    into money_tbl_02 values(100001, 20160002, 1000, 4, 4000, 'A002', '2016-01-01')
-    into money_tbl_02 values(100001, 20160003, 500, 3, 1500, 'A008', '2016-01-01')
-    into money_tbl_02 values(100002, 20160004, 2000, 1, 2000, 'A004', '2016-01-02')
-    into money_tbl_02 values(100002, 20160005, 500, 1, 500, 'A001', '2016-01-03')
-    into money_tbl_02 values(100003, 20160006, 1500, 2, 3000, 'A003', '2016-01-03')
-    into money_tbl_02 values(100004, 20160007, 500, 2, 1000, 'A001', '2016-01-04')
-    into money_tbl_02 values(100004, 20160008, 300, 1, 300, 'A005', '2016-01-04')
-    into money_tbl_02 values(100004, 20160009, 600, 1, 600, 'A006', '2016-01-04')
-    into money_tbl_02 values(100004, 20160010, 3000, 1, 3000, 'A007', '2016-01-06')
+    into money_tbl_02 values(20160001, 100001, 500, 5, 2500, 'A001', '2016-01-01')
+    into money_tbl_02 values(20160002, 100001, 1000, 4, 4000, 'A002', '2016-01-01')
+    into money_tbl_02 values(20160003, 100001, 500, 3, 1500, 'A008', '2016-01-01')
+    into money_tbl_02 values(20160004, 100002, 2000, 1, 2000, 'A004', '2016-01-02')
+    into money_tbl_02 values(20160005, 100002, 500, 1, 500, 'A001', '2016-01-03')
+    into money_tbl_02 values(20160006, 100003, 1500, 2, 3000, 'A003', '2016-01-03')
+    into money_tbl_02 values(20160007, 100004, 500, 2, 1000, 'A001', '2016-01-04')
+    into money_tbl_02 values(20160008, 100004, 300, 1, 300, 'A005', '2016-01-04')
+    into money_tbl_02 values(20160009, 100004, 600, 1, 600, 'A006', '2016-01-04')
+    into money_tbl_02 values(20160010, 100004, 3000, 1, 3000, 'A007', '2016-01-06')
 select * from dual;
+COMMIT;
+SELECT * FROM MONEY_TBL_02;
